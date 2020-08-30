7a747e99216765cffa7a248d0ddb745bf75d20d9
mounikk <monikakaushik@monikas-MacBook-Air.local>
2020-08-31 00:54:22 +0530

    change2

--- src/geneic/Sum.java
+++ src/geneic/Sum.java
@@ -7,14 +7,20 @@ public class Sum {
 		
 		System.out.println(5+8);
 <<<<<<< HEAD
+<<<<<<< HEAD
 		// Making "third" commit in master branch
 		
 =======
+=======
+>>>>>>> change2
 		// Trying rebase vs merge 
 		// git merge --squash merge
 		// git commit -m "merged into master branch"
 	      
+<<<<<<< HEAD
 >>>>>>> FirstBranch
+=======
+>>>>>>> change2
 
 	}
 
